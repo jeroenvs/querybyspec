454494918247b7465872729d6be79ec13da427ca
Jeroen van Schagen <jeroen@it-techniek.nl>
2011-01-07 09:55:31 +0100

    removed the interface layer on generic repo, not needed, no specific implementation logic is inside, just delegation

--- src/main/java/org/jeroen/ddd/repository/GenericRepository.java
+++ src/main/java/org/jeroen/ddd/repository/GenericRepository.java
@@ -1,25 +1,65 @@
 package org.jeroen.ddd.repository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import org.jeroen.ddd.specification.Specification;
 
-public interface GenericRepository<T> extends FlexibleRepository<T> {
+/**
+ * Generic repository, can be used to store and retrieve entities of any type. The
+ * implementation delegates all data behaviour to an internal strategy, which can
+ * be manipulated during runtime.
+ * 
+ * @author Jeroen van Schagen
+ * @since 29-12-2010
+ *
+ * @param <T> type of entities being managed
+ */
+public class GenericRepository<T> implements FlexibleRepository<T> {
+    private RepositoryStrategy<T> strategy;
+
+    /**
+     * Construct a new {@link GenericRepository}.
+     */
+    public GenericRepository() {
+        super();
+    }
+
+    /**
+     * Construct a new {@link GenericRepository}, for the specified entities.
+     * @param strategy the strategy that should be applied
+     */
+    public GenericRepository(RepositoryStrategy<T> strategy) {
+        super();
+        this.strategy = strategy;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<T> matching(Specification<T> specification) {
+        return strategy.matching(specification);
+    }
 
     /**
      * Count how many entities match a specification.
      * @param specification
      * @return
      */
-    long countBy(Specification<T> specification);
+    public long countBy(Specification<T> specification) {
+        return strategy.countBy(specification);
+    }
 
     /**
      * Determine if any of our entities match a specification.
      * @param specification
      * @return
      */
-    boolean hasAny(Specification<T> specification);
+    public boolean hasAny(Specification<T> specification) {
+        return strategy.hasAny(specification);
+    }
 
     /**
      * Store an entity, enabling it to be accessed.
@@ -27,7 +67,9 @@ public interface GenericRepository<T> extends FlexibleRepository<T> {
      * @param entity
      * @return
      */
-    <R extends T> R add(R entity);
+    public <R extends T> R add(R entity) {
+        return strategy.add(entity);
+    }
 
     /**
      * Store a collection of entities, enabling them to be accessed.
@@ -35,19 +77,40 @@ public interface GenericRepository<T> extends FlexibleRepository<T> {
      * @param entities
      * @return
      */
-    <R extends T> List<R> addAll(Collection<R> entities);
+    public <R extends T> List<R> addAll(Collection<R> entities) {
+        List<R> result = new ArrayList<R>();
+        for (R entity : entities) {
+            result.add(this.add(entity));
+        }
+        return result;
+    }
 
     /**
      * Remove an entities, preventing it to be accessed.
      * @param entity
      */
-    void remove(T entity);
+    public void remove(T entity) {
+        strategy.remove(entity);
+    }
 
     /**
      * Remove any entity that matches the provided specification.
      * @param specification
      * @return list of removed entities
      */
-    List<T> removeAny(Specification<T> specification);
+    public List<T> removeAny(Specification<T> specification) {
+        List<T> entities = this.matching(specification);
+        for (T entity : entities) {
+            this.remove(entity);
+        }
+        return entities;
+    }
+
+    // Attribute access
+
+    public GenericRepository<T> setStrategy(RepositoryStrategy<T> strategy) {
+        this.strategy = strategy;
+        return this;
+    }
 
 }
--- src/test/java/org/jeroen/ddd/repository/RepositoryTest.java
+++ src/test/java/org/jeroen/ddd/repository/RepositoryTest.java
@@ -14,14 +14,14 @@ public class RepositoryTest {
     public void testInMemory() {
         Post firstPost = new Post().setMessage("first");
         Post secondPost = new Post().setMessage("second");
-        GenericRepository<Post> repository = new GenericRepositoryImpl<Post>(new InMemoryRepositoryStrategy<Post>());
-        Specification<Post> specification = new EqualitySpecification<Post>("message", "first");
+        GenericRepository<Post> posts = new GenericRepository<Post>(new InMemoryRepositoryStrategy<Post>());
+        Specification<Post> isFirst = new EqualitySpecification<Post>("message", "first");
         // Repository is empty, thus no matching entities should be found
-        Assert.assertTrue(repository.matching(specification).isEmpty());
+        Assert.assertTrue(posts.matching(isFirst).isEmpty());
         // Append our 'first' and 'second' post to the repository, enabling them to be found
-        repository.addAll(Arrays.asList(firstPost, secondPost));
+        posts.addAll(Arrays.asList(firstPost, secondPost));
         // Now the 'first' post should be found because it satisfies our equality specification
-        Assert.assertEquals(Arrays.asList(firstPost), repository.matching(specification));
+        Assert.assertEquals(Arrays.asList(firstPost), posts.matching(isFirst));
     }
 
 }
